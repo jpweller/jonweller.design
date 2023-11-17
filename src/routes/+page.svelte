7d5f6f4fa75761cf5f1551dcafbd61e24678f165
Jonathon Weller <jonathon.weller@gmail.com>
2023-11-17 12:10:10 -0500

    Organize styles, add button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,19 @@
 <main>
   <header>
-    <!-- <h1>Good morning,<br />I&rsquo;m Jonathon.</h1> -->
+    <h1>Good morning,<br />I&rsquo;m Jonathon.</h1>
+    <p>
+      UX <span class="color-primary">+</span> Visual Design <span class="color-primary">+</span> Code
+    </p>
+    <div class="buttons">
+      <a href="/contact" class="button button--primary">Get in touch</a>
+      <a href="/work" class="button button--secondary">See my Work</a>
+    </div>
   </header>
+  <section class="">
+    <h2 class="text-eyebrow">About</h2>
+    <p class="text-heading text-2xl">
+      I&rsquo;m a designer and developer with a passion for creating beautiful, intuitive, and
+      accessible experiences.
+    </p>
+  </section>
 </main>
