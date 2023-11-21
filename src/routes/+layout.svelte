bc6287e0bcab149a23cee8b36e575f98b15c016f
Jonathon Weller <jonathon.weller@gmail.com>
2023-11-21 18:57:12 -0500

    update footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,115 +1,69 @@
 <script lang="ts">
   import Swoosh from '$lib/components/Swoosh.svelte';
+  import ThemeToggleButton from '$lib/components/theme-toggle/ThemeToggleButton.svelte';
   import '../main.scss';
 </script>
 
 <slot />
 
 <footer>
-  <div class="footer section">
+  <div class="section">
     <div class="section-content">
-      <div class="footer-content-left">
+      <div class="header">
         <Swoosh />
-        <div class="footer-content-left-text">
-          <p class="text-xl">
-            JP Weller is a freelance designer and developer based in New York City and Chincoteague,
-            Virginia.
-          </p>
-        </div>
+        <ThemeToggleButton />
       </div>
-      <div class="footer-content-right">
-        <div class="footer-content-right-links">
-          <a href="https://twitter.com/jpweller">Twitter</a>
-          <a href="https://github.com/jpweller">GitHub</a>
-          <a href="https://www.linkedin.com/in/jpweller/">LinkedIn</a>
-          <a href="https://www.instagram.com/jpweller/">Instagram</a>
-        </div>
-        <div class="footer-content-right-email">
-          <a href="mailto:jon@jonweller.design">Email</a>
-        </div>
+      <div class="mega-buttons">
+        <a href="/work" class="button button--inverse text-heading text-2xl">See my work</a>
+        <a href="/contact" class="button button--inverse text-heading text-2xl">Contact</a>
+      </div>
+      <p class="text-heading text-2xl">
+        Jonathon Weller is a designer and software developer based in Chincoteague, Virginia and in
+        New York City.
+      </p>
+      <div class="links">
+        <a target="_blank" href="https://github.com/jpweller">GitHub</a>
+        <a target="_blank" href="https://www.linkedin.com/in/jpweller/">LinkedIn</a>
+        <a target="_blank" href="https://www.instagram.com/jpweller/">Instagram</a>
+        <a target="_blank" href="mailto:jon@jonweller.design">Email</a>
+      </div>
+      <div class="links text-sm">
+        <p>© Copyright JP Weller Designs, LLC</p>
+        <a href="/colophone">Colophone</a>
       </div>
     </div>
   </div>
 </footer>
 
 <style lang="scss">
-  footer {
+  .section {
     background-color: var(--color-primary);
-    color: var(--color-white);
-    padding: 2rem 0;
+    color: var(--color-text-inverse);
   }
 
-  .footer-content-left {
+  .section-content {
+    align-items: normal;
+    gap: max(15vh, 2rem);
+  }
+
+  .header {
+    display: flex;
+    justify-content: space-between;
+    align-items: space-between;
+  }
+
+  .mega-buttons {
     display: flex;
     flex-direction: column;
-    justify-content: space-between;
-    align-items: flex-start;
-    gap: 4rem;
-    width: 100%;
-    max-width: 48rem;
+    gap: 1rem;
   }
 
-  .footer-content-left-text {
-    width: 100%;
-    max-width: 400px;
-  }
-
-  .footer-content-right {
+  .links {
     display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    width: 100%;
-    max-width: 48rem;
-  }
+    gap: 1rem;
 
-  .footer-content-right-links {
-    display: flex;
-    justify-content: space-between;
-    width: 100%;
-    max-width: 400px;
-  }
-
-  .footer-content-right-email {
-    width: 100%;
-    max-width: 200px;
-  }
-
-  .footer-content-right-links a {
-    color: var(--color-white);
-    text-decoration: none;
-    font-size: 1.25rem;
-  }
-
-  .footer-content-right-email a {
-    color: var(--color-white);
-    text-decoration: none;
-    font-size: 1.25rem;
-  }
-
-  @media (max-width: 768px) {
-    .footer-content {
-      flex-direction: column;
-      align-items: center;
-    }
-
-    .footer-content-left {
-      margin-bottom: 2rem;
-    }
-
-    .footer-content-right {
-      width: 100%;
-      max-width: 400px;
-    }
-
-    .footer-content-right-links {
-      justify-content: space-between;
-      width: 100%;
-      max-width: 400px;
-    }
-
-    .footer-content-right-email {
-      width: 100%;
-      max-width: 200px;
+    a {
+      color: var(--color-text-inverse);
     }
   }
 </style>
