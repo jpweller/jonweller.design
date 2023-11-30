20c1b68a04be6fb622de7aa09738f272b9cd2016
Jonathon Weller <jonathon.weller@gmail.com>
2023-11-30 14:37:42 -0500

    Added error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import { page } from '$app/stores';
+  import Header from '$lib/components/Header.svelte';
+  import Footer from '$lib/components/Footer.svelte';
+  import '../main.scss';
+</script>
+
+<Header />
+<main class="section">
+  <div class="section-content center">
+    <div class="text text-center">
+      <h1 class="status">{$page.status}</h1>
+      {#if $page.error}
+        <p class="text-xl">{$page.error.message}</p>
+      {/if}
+    </div>
+  </div>
+</main>
+<Footer />
+
+<style lang="scss">
+  .status {
+    font-size: max(7em, 20vw);
+  }
+</style>
