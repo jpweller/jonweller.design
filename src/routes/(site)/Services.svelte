1521c53fc30ec46cae9d65071900e8361c187d3c
Jonathon Weller <jonathon.weller@gmail.com>
2023-12-04 17:45:12 -0500

    glow effect

--- src/routes/(site)/Services.svelte
+++ src/routes/(site)/Services.svelte
@@ -35,41 +35,55 @@
       animationData: devAnimationData
     });
   });
+
+  let container: HTMLElement;
+  let mousePosition = { x: 0, y: 0 };
+
+  function handleMousemove(event: MouseEvent) {
+    var rect = container.getBoundingClientRect();
+    mousePosition = { x: event.clientX - rect.x, y: event.clientY - rect.y };
+  }
 </script>
 
 <Section title="Services">
-  <div class="auto-grid auto-grid--sm">
-    <div class="stack gap-2">
+  <div
+    class="container auto-grid auto-grid--sm gap-1"
+    bind:this={container}
+    on:mousemove={handleMousemove}
+    role="presentation"
+    style="--mouse-x: {mousePosition.x}px; --mouse-y: {mousePosition.y}px"
+  >
+    <div class="card stack gap-2">
       <div role="img" aria-labelledby="ux-design" class="img" bind:this={uxContainer}></div>
-      <h3 id="ux-design" class="text-heading text-xl">UX Design</h3>
+      <h3 id="ux-design" class="text-heading text-xl">UX design</h3>
       <ul class="stack color-secondary">
-        <li>User Experience Design</li>
-        <li>Marketing Sites</li>
-        <li>New User Funnels</li>
-        <li>Enterprise Dashboards</li>
-        <li>Data Visualization</li>
-        <li>Design Systems</li>
-        <li>Lottie Animations</li>
+        <li>User experience design</li>
+        <li>Marketing sites</li>
+        <li>New user funnels</li>
+        <li>Enterprise dashboards</li>
+        <li>Data visualization</li>
+        <li>Design systems</li>
+        <li>Lottie animations</li>
       </ul>
     </div>
-    <div class="stack gap-2">
+    <div class="card stack gap-2">
       <div role="img" aria-labelledby="web-dev" class="img" bind:this={devContainer}></div>
-      <h3 id="web-dev" class="text-heading text-xl">Web Dev</h3>
+      <h3 id="web-dev" class="text-heading text-xl">Web dev</h3>
       <ul class="stack color-secondary">
         <li>Webflow</li>
         <li>Svelte</li>
         <li>Sanity</li>
         <li>Cloudinary</li>
-        <li>Design Systems (React + Web components)</li>
-        <li>Web Animations</li>
+        <li>Design systems (React + Web components)</li>
+        <li>Web animations</li>
       </ul>
     </div>
-    <div class="stack gap-2">
+    <div class="card stack gap-2">
       <div role="img" aria-labelledby="branding" class="img" bind:this={brandingContainer}></div>
       <h3 id="branding" class="text-heading text-xl">Branding</h3>
       <ul class="stack color-secondary">
-        <li>Logo & Identity</li>
-        <li>Brand Books & Style Guides</li>
+        <li>Logo & identity</li>
+        <li>Brand books & style guides</li>
         <li>Illustration</li>
       </ul>
     </div>
@@ -80,5 +94,53 @@
   .img {
     height: 5rem;
     width: 6.25rem;
+  }
+
+  .container {
+    position: relative;
+    --scale: 0;
+
+    &:hover {
+      --scale: 1;
+    }
+
+    &:has(.card:hover) .card::after {
+      opacity: 1;
+    }
+  }
+
+  .card {
+    padding: 1rem;
+    background-color: var(--color-bg);
+    overflow: hidden;
+    clip-path: polygon(0 0, 100% 0, 100% calc(100% - 4rem), calc(100% - 4rem) 100%, 0 100%);
+
+    @media (min-width: 40rem) {
+      padding: 2rem;
+
+      &::after {
+        content: '';
+        position: absolute;
+        top: 0;
+        left: 0;
+        height: 20rem;
+        width: 20rem;
+        background-color: var(--color-primary);
+        transform: translate(calc(-50% + var(--mouse-x)), calc(-50% + var(--mouse-y)))
+          scale(var(--scale));
+        opacity: 0;
+        pointer-events: none;
+        transition:
+          transform 0.2s ease-out,
+          opacity 0.2s ease-out;
+        border-radius: 50%;
+        filter: blur(200px);
+        z-index: -1;
+
+        @media prefers-reduced-motion {
+          display: none;
+        }
+      }
+    }
   }
 </style>
